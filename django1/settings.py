3630bc9cd91227c9fb1c8e47a3ce04ca10d3872a
innocent okinda <olindainnocent@gmail.com>
2023-05-10 20:27:28 +0300

    first commit

---
+++ django1/settings.py
@@ -0,0 +1,125 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'c+#8x$+zfq00i(z@6_81ht9i-0dcp53iyl_bp$mr^f22k600i*'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'register',
+    'inventory'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'orthosie.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+WSGI_APPLICATION = 'orthosie.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'orthosie/orthosie.db'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/New_York'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), '../static').replace('\\', '/'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+TAX = .075
+PRINTER = '/dev/null'
+RECEIPT_HEADER = ['Header 1', 'Header 2']
+RECEIPT_FOOTER = ['Footer 1', 'Footer 2']
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
---
+++ django1/urls.py
@@ -0,0 +1,53 @@
+#    Copyright 2013 Jack David Baucum
+#
+#    This file is part of Orthosie.
+#
+#    Orthosie is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Orthosie is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Orthosie.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.conf.urls import include, url
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import routers
+from inventory.api_views import ItemViewSet, GroceryViewSet, ProduceViewSet, VendorViewSet
+from register.api_views import ShiftViewSet, TransactionViewSet, LineItemViewSet, TenderViewSet
+from inventory import urls as inventoryUrls
+from register import urls as registerUrls
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    url(r'^register/', include(registerUrls)),
+    url(r'^inventory/', include(inventoryUrls)),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'api'])
+
+router = routers.DefaultRouter()
+router.register(r'items', ItemViewSet)
+router.register(r'groceries', GroceryViewSet)
+router.register(r'produce', ProduceViewSet)
+router.register(r'vendors', VendorViewSet)
+router.register(r'shifts', ShiftViewSet)
+router.register(r'transactions', TransactionViewSet)
+router.register(r'line-items', LineItemViewSet)
+router.register(r'tenders', TenderViewSet)
+
+urlpatterns += [
+    url(r'^', include(router.urls)),
+    url(
+        r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')
+    )
+]
